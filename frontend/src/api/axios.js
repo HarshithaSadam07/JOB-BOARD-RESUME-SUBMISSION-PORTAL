ccbea0094eb42336efea481d9b61592765140b11
HarshithaSadam07 <harshithasadam2006@gmail.com>
2025-11-08 11:35:17 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://job-board-resume-submission-portal.onrender.com/api",
 });
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token");
